c999eedcc3395754555a4677eb12dda55a2a4a74
Zaid Munir <zaid@scytalelabs.com>
2022-09-04 15:06:58 +0500

    cep47 scripts refactored

--- JsClients/casper-cep47/scripts/mintOneToken.ts
+++ JsClients/casper-cep47/scripts/mintOneToken.ts
@@ -1,8 +1,9 @@
-import {mintOneToken} from './installed';
+import {Cep47} from './cep47';
 let map = new Map<string,string>().set(process.argv[3],process.argv[4]);
 let arrayOfMaps = [map];
 let tokenIds = [process.argv[2]];
-mintOneToken(tokenIds,arrayOfMaps);
+let cep47 = new Cep47();
+cep47.mint(tokenIds,arrayOfMaps);
 
 /*
     "mintOne description"   :   "use it to mint only one NFT with given ID, 
