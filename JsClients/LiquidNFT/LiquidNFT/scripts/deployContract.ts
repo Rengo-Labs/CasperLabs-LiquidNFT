dcbb12f1fb55860023b909159011dc8d626e9f61
Zaid Munir <zaid@scytalelabs.com>
2022-09-05 15:17:12 +0500

    both LNFT clients meged into one

---
+++ JsClients/LiquidNFT/LiquidNFT/scripts/deployContract.ts
@@ -0,0 +1,13 @@
+import {LiquidNFT} from "./lnft";
+let liquidNFT = new LiquidNFT();
+liquidNFT.deployContract(process.argv[2]!);
+
+/*
+    "Script donateToLocker comments": {
+        "Description" : "use it to donate to Locker",
+        "Syntax" : "npm run donateToLocker <lockerPackageHash> <donationAmount>",
+        "Example" : "npm run donateToLocker 841f21fefb97f759a555255b0a414fda519c6deba4007c3a17ada7dc233552d8 100"
+    },
+*/
+
+//hash-8f07a23bf1b230657f0c7e8475f25dfe24526edfb70a0475b7bee1ace658ec7f
--- JsClients/LiquidNFT/LiquidNFT/src/types.d.ts
+++ JsClients/LiquidNFT/LiquidNFT/src/types.d.ts
@@ -2,3 +2,10 @@ import { CLAccountHash, CLByteArray, CLPublicKey } from "casper-js-sdk";
 
 export type RecipientType = CLPublicKey | CLAccountHash | CLByteArray;
 
+
+import {LIQUIDNFTEvents} from "./constants";
+
+export interface IPendingDeploy {
+  deployHash: string;
+  deployType: LIQUIDNFTEvents;
+}
